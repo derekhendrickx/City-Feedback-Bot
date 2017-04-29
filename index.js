1db33aa2b3a5330f49335b6e0403927a127164e3
Derek Hendrickx <derek@hendrickx.me>
2017-04-29 15:56:50 -0400

    Convert to ES6

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ controller.hears(['hello'], 'message_received', (bot, message) => {
   bot.reply(message, 'Hey there.');
 });
 
-controller.hears(['cookies'], 'message_received', function(bot, message) {
-  bot.startConversation(message, function(err, convo) {
+controller.hears(['cookies'], 'message_received', (bot, message) => {
+  bot.startConversation(message, (err, convo) => {
     convo.say('Did someone say cookies!?!!');
-    convo.ask('What is your favorite type of cookie?', function(response, convo) {
+    convo.ask('What is your favorite type of cookie?', (response, convo) => {
       convo.say('Golly, I love ' + response.text + ' too!!!');
       convo.next();
     });
