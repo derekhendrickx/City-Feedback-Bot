b87d7cc1c336f1405a1f0ec74c111c407aa1d4fc
Derek Hendrickx <derek@hendrickx.me>
2017-04-30 11:53:28 -0400

    Add greeting

--- index.js
+++ index.js
@@ -16,23 +16,34 @@ controller.setupWebserver(process.env.PORT || 3000, (err, webserver) => {
 });
 
 controller.api.thread_settings.greeting('Hey thanks for coming to talk to us. We are Sustaincity and we want to create cleaner parks that we can all roll around in.');
+controller.api.messenger_profile.get_started()
 
 // this is triggered when a user clicks the send-to-messenger plugin
 controller.on('facebook_optin', (bot, message) => {
   bot.reply(message, 'Welcome to my app!');
 });
 
-controller.on('message_received', (bot, message) => {
-  if (!message.text) {
-    console.log(message.attachments[0]);
-    console.log(message.attachments[0].payload);
-    if (message.attachments && message.attachments[0]) {
-      // controller.trigger(message.attachments[0].type + '_received', [bot, message]);
-      bot.say('Your picture has been received, thank you.');
-      return false;
-    }
+controller.on('facebook_referral', (bot, message) => {
+  bot.reply(message, 'Welcome to my app!');
+});
+
+controller.on('facebook_postback', function(bot, message) {
+  if (message.payload == 'get_started') {
+    bot.reply(message, 'You ate the chocolate cookie!')
   }
 });
+
+// controller.on('message_received', (bot, message) => {
+//   if (!message.text) {
+//     console.log(message.attachments[0]);
+//     console.log(message.attachments[0].payload);
+//     if (message.attachments && message.attachments[0]) {
+//       // controller.trigger(message.attachments[0].type + '_received', [bot, message]);
+//       bot.say('Your picture has been received, thank you.');
+//       return false;
+//     }
+//   }
+// });
 
 controller.hears(['feedback'], 'message_received', function(bot, message) {
   bot.startConversation(message, (err, convo) => {
@@ -71,7 +82,7 @@ controller.hears(['feedback'], 'message_received', function(bot, message) {
 
       convo.ask(opinion, (response, convo) => {
         convo.next();
-        
+
         convo.ask('Why ? Send us a picture of the park.', (response, convo) => {
           console.log('Picture', response.attachments[0].payload);
           convo.say('Thank you!');
